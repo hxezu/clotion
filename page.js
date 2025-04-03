a0cc961c6946b1575e37435a5a31407f33fecd29
ty <composedkim@naver.com>
2025-04-03 16:21:08 +0900

    api 연결

--- page.js
+++ page.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+import { makeFile,fetchUserData, updateDocument, fetchUserContent } from "./fetch.js"
+
+document.addEventListener('DOMContentLoaded', async() => {
   const addPageBtn = document.getElementById('addPageBtn')
   const pageList = document.querySelector('.pageList')
   const pageContentElement = document.querySelector('#editor')
   const pageTitleElement = document.querySelector('.pageTitle')
 
+
+  let currentDocumentId = null;
+  await renderDocumentList()
   //새 페이지 버튼 이벤트
   addPageBtn.addEventListener('click', () => {
     const newPage = document.createElement('li')
@@ -21,21 +26,26 @@ document.addEventListener('DOMContentLoaded', () => {
     newPage.appendChild(pageTitle)
     pageList.appendChild(newPage)
 
-    //페이지 아이디 생성
-    const pageId = Date.now()
-    newPage.dataset.pageId = pageId
-    pageTitleElement.dataset.pageId = pageId
+    makeFile().then((newId) => {
+      if(newId){
+        currentDocumentId = newId
+        const pageId = newId
+        newPage.id = pageId
+        pageTitleElement.id = pageId
 
-    newPage.addEventListener('click', () => {
-      openPage(pageTitle.textContent, pageId, newPage)
+        newPage.addEventListener('click', () => {
+          openPage(pageTitle.textContent, pageId, newPage)
+        })
+      }
     })
+
   })
 
   //페이지 제목 바꾸면 사이드 바에도 같이 변경
   pageTitleElement.addEventListener('input', () => {
     const currentPageId = pageTitleElement.dataset.pageId
     const sidebarPage = document.querySelector(
-      `.pageItem[data-page-id="${currentPageId}"] span`
+      `.pageItem[id="${currentPageId}"] span`
     )
 
     if (sidebarPage) {
@@ -74,6 +84,15 @@ document.addEventListener('DOMContentLoaded', () => {
     if (pageTitleElement.textContent.trim() === '') {
       pageTitleElement.textContent = 'New Page'
     }
+
+  })
+
+  pageContentElement.addEventListener('focusout', () => {
+    saveDocument()
+  })
+
+  pageTitleElement.addEventListener('focusout', () => {
+    saveDocument()
   })
 
   //제목에 엔터키 입력 방지
@@ -83,10 +102,73 @@ document.addEventListener('DOMContentLoaded', () => {
       pageTitleElement.blur() //입력 종료
     }
   })
+
+
+  //사이드바 랜더링
+async function renderDocumentList() {
+  const documents = await getAllIdsAndTitles();
+  const existingLi = pageList.querySelectorAll('li');
+  pageList.innerHTML = '';
+  existingLi.forEach((li) => pageList.appendChild(li));
+  documents.forEach((doc) => {
+    addDocumentToLeftPanel(doc.id, doc.title);
+  });
+ // console.log(documents);
+}
+
+// title, id 불러오기
+async function getAllIdsAndTitles() {
+  const response = await fetchUserData();
+  return response.map((doc) => ({
+    id: doc.id,
+    title: doc.title || '(제목 없음)',
+  }));
+}
+
+//사이드바 추가
+ function addDocumentToLeftPanel(id, title) {
+  const newPage = document.createElement('li')
+  newPage.classList.add('pageItem')
+  newPage.id = id;
+  currentDocumentId = id
+
+  const docIcon = document.createElement('img')
+  docIcon.src = 'assets/docIcon.svg'
+  docIcon.alt = 'doc-Icon'
+  docIcon.classList.add('docIcon')
+
+  const pageTitle = document.createElement('span')
+  pageTitle.textContent = title
+
+  newPage.appendChild(docIcon)
+  newPage.appendChild(pageTitle)
+  pageList.appendChild(newPage)
+
+  newPage.addEventListener('click', async () => {
+    const pageContent = await fetchUserContent(id)
+    console.log(pageContent)
+    openPage(pageTitle.textContent, id, pageContent)
+    currentDocumentId = id
+    saveDocument()
+  })
+}
+
+//저장 함수
+async function saveDocument() {
+  const selectedId = document.getElementById(currentDocumentId)
+  const selectedTitle = selectedId.querySelector('span').textContent.trim()
+  const selectedContent = document.getElementById("editor").textContent
+  const updatedData = { title: selectedTitle, content: selectedContent }
+
+   const success = await updateDocument(selectedId.id, updatedData);
+   if (success) {
+     //console.log("성공")
+   }
+}
 })
 
 //페이지 열기
-function openPage(pageTitle, pageId) {
+function openPage(pageTitle, pageId,pageContent) {
   const pageTitleElement = document.querySelector('.pageTitle')
   const pageContentElement = document.querySelector('#editor')
 
@@ -97,6 +179,11 @@ function openPage(pageTitle, pageId) {
 
   pageTitleElement.textContent = pageTitle
   pageTitleElement.dataset.pageId = pageId
-  pageContentElement.textContent = `내용을 입력하세요...`
+  if(pageContent === null){
+     pageContentElement.textContent = `내용을 입력하세요...`
+  }else{
+    pageContentElement.textContent = pageContent
+  }
   history.pushState({ page: pageTitle }, '', `#${pageTitle}`)
 }
+
